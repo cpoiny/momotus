b81f4082e3fef1d86857c2160dbd603a0916186e
Cynthia Poiny <cynthia.poiny@gmail.com>
2024-01-15 10:50:03 +0100

    creation des composants et mise a jour d angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameComponent } from './components/game/game.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
